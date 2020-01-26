144712288df061bf17d74f01acad656a13089426
Rodrigo Pina Lozano (CI/DAD5.1) <rodrigo.pina@de.bosch.com>
2020-01-26 17:25:03 +0100

    sends request to ifttt with XMLHttpRequest

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const express = require('express');
 const app = express();
 const AWS = require('aws-sdk');
 const uuid = require('node-uuid');
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 const { POSITIONS_TABLE, POIS_TABLE, IS_OFFLINE } = process.env;
+const endpointIFTTT = 'https://maker.ifttt.com/trigger/transition/with/key/bJQmYS7tQFaNdmfLChbr0b   ';
 
 const dynamoDb = IS_OFFLINE === 'true' ?
     new AWS.DynamoDB.DocumentClient({
@@ -89,6 +91,113 @@ app.post('/poi', (req, res) => {
     });
 });
 
+
+app.post('/owntracks', (req, res) => {
+    let _type = req.body._type;
+    if (typeof _type == 'undefined') {
+        console.log('Error creating Owntrack record');
+        res.status(500).json({ error: 'Could not create record' });
+    }
+    if (_type == 'waypoints' || _type == "dump" || _type == "lwt" || _type == "configuration" || _type == "beacon" || _type == "cmd" || _type == "steps" || _type == "card" || _type == "encrypted") {
+        console.log("Received command: " + _type + " in body " + JSON.stringify(req.body));
+        res.status(200).json({ status: 'ok' });
+    }
+
+    if (_type == 'transition') {
+        let { tid, desc, event } = req.body;
+        console.log("Received command: " + _type + " in body " + JSON.stringify(req.body));
+        
+        var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
+        xmlhttp.open("POST", endpointIFTTT);
+        xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+        //TODO: Improve localization
+        if (event == "enter") {
+            event = "entró a";
+        } else if (event == "leave") {
+            event = "salio de";
+        }
+        
+        xmlhttp.send(JSON.stringify({ value1: tid, value2: desc, value3: event }));
+
+        res.status(200).json({ status: 'ok' });
+    }
+
+    if (_type == 'location') {
+        let { tst, lat, lon, topic } = req.body;
+        latitude = lat;
+        longitude = lon;
+        positionId = uuid.v4();
+        timestamp = tst;
+        name = topic.split('/')[1].toLowerCase();
+
+        const params = {
+            TableName: POSITIONS_TABLE,
+            Item: {
+                positionId,
+                name,
+                latitude,
+                longitude,
+                timestamp,
+            },
+        };
+        dynamoDb.put(params, (error) => {
+            if (error) {
+                console.log('Error creating position: ', error);
+                res.status(400).json({ error: 'Could not create position' });
+            }
+            const positionParams = {
+                TableName: POSITIONS_TABLE,
+            };
+            dynamoDb.scan(positionParams, (error, result) => {
+                if (error) {
+                    res.status(400).json({ error: 'Error retrieving positions' });
+                }
+
+                for (let index = 0; index < result.Items.length; index++) {
+                    result.Items[index]._type = "location";
+                    result.Items[index].tid = result.Items[index].name.substring(0, 2);
+                    result.Items[index].tst = result.Items[index].timestamp;
+                    result.Items[index].lat = result.Items[index].latitude;
+                    result.Items[index].lon = result.Items[index].longitude;
+                    result.Items[index].topic = "owntracks/" + result.Items[index].name + "/iphone"
+                };
+
+                const { Items: positions } = result;
+                res.json(positions);
+            })
+        });
+    } else if (_type == 'waypoint') {
+        let { tst, desc, lat, lon } = req.body;
+        latitude = lat;
+        longitude = lon;
+        positionId = uuid.v4();
+        timestamp = tst;
+        poiname = desc;
+
+        if (poiname == "") {
+            poiname = "undefined"
+        }
+
+        const params = {
+            TableName: POIS_TABLE,
+            Item: {
+                positionId,
+                poiname,
+                latitude,
+                longitude,
+                timestamp,
+            },
+        };
+        dynamoDb.put(params, (error) => {
+            if (error) {
+                console.log('Error creating POI: ', error);
+                res.status(400).json({ error: 'Could not create POI' });
+            }
+            res.json({ positionId, poiname, latitude, longitude, timestamp });
+        });
+    }
+});
 
 app.get('/position/:name', (req, res) => {
     const { name } = req.params;
@@ -210,11 +319,11 @@ app.get('/locate/:name', (req, res) => {
                 } else {
                     res.contentType = "application/json"
                     jsonResult.name = name;
-                    jsonResult.distance = gdistance("48.814130","9.146436",result.Item.latitude,result.Item.longitude);    
-                    
+                    jsonResult.distance = gdistance("48.814130", "9.146436", result.Item.latitude, result.Item.longitude);
+
                     var found = false;
                     data.Items.forEach(function (item) {
-                        if (isWhithinRadiusOfPOI(result.Item.latitude, result.Item.longitude, item.latitude, item.longitude, 50)) {
+                        if (isWhithinRadiusOfPOI(result.Item.latitude, result.Item.longitude, item.latitude, item.longitude, 20)) {
                             jsonResult.poi = item.poiname;
                             found = true;
                         }
